3c36c309ef06f381e323a7db2311a7370e77c6b0
chenjinshu <chenjingshu@pashanhu.cn>
2018-03-06 14:54:59 +0800

    no message

--- src/com/cjs/array/ArrayTest.java
+++ src/com/cjs/array/ArrayTest.java
@@ -9,7 +9,6 @@ import java.util.Date;
 
 public class ArrayTest {
 
-<<<<<<< HEAD
     public static void  main(String[] args) throws ParseException {
     	BigDecimal expenseNormal = null;
 		BigDecimal expenseDestination = null;
@@ -18,12 +17,6 @@ public class ArrayTest {
 		Date beginTime = sd.parse("2017-12-31 09:00:00");
 		Date endTime = sd.parse("2018-01-01 07:00:00");
 		
-		BigDecimal a = new BigDecimal(2);
-		BigDecimal b = new BigDecimal(2.0);
-		System.out.println(a.compareTo(b));
-		
-=======
-    public static void  main(String[] args) {
         int[] a = new int[]{1,2,3,4,5};
         // int[] a = new int[5]{1,2,3,4,5};    // 错误，在数组声明同时赋值时不能指定数组长度
 
@@ -33,6 +26,5 @@ public class ArrayTest {
 
         String ai = "";
         
->>>>>>> a13fe4b10876783a78d9f1f5a64df6d05dfd36ca
     }
 }
