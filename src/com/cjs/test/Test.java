1c030579bf6564d9f52f8975d423161fd2fbaedb
chenjinshu <chenjingshu@pashanhu.cn>
2018-03-22 17:29:42 +0800

    no message

--- src/com/cjs/test/Test.java
+++ src/com/cjs/test/Test.java
@@ -2,27 +2,16 @@ package com.cjs.test;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 
 public class Test {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
     	
-    	List<String> list = new ArrayList<>();
-    	setList(list);
-    	System.out.println(list.get(0));
+    	List<String> list = Collections.emptyList();
+    	System.out.println(list.size());
     	
-    }
-    
-    public static void setList(List<String> list) {
-    	list.add("aaa");
-=======
-
-        int a = 4;
-        int b = 5;
-        System.out.println(a);
->>>>>>> 60d5a7d3f7a5afc427d76d6dde7d715700b867ef
     }
 
 }
