32862c929db05f6dfcaa95d0c9a062b266aaac27
chenjinshu <chenjingshu@pashanhu.cn>
2018-03-23 17:36:17 +0800

    no message

--- src/com/cjs/concurrentPackageTest/ReentrantLockTest.java
+++ src/com/cjs/concurrentPackageTest/ReentrantLockTest.java
@@ -6,7 +6,7 @@ public class ReentrantLockTest implements Runnable {
 
     static int i;
 
-    static ReentrantLock reentrantLock = new ReentrantLock();
+    static ReentrantLock reentrantLock = new ReentrantLock();   // 可重入锁（同一线程不受锁的限制，但不同线程会受到限制）
 
     @Override
     public void run () {
@@ -15,7 +15,7 @@ public class ReentrantLockTest implements Runnable {
             try {
                 i++;
             } finally {
-                reentrantLock.unlock();   // 解锁，在解锁前任何线程都无法进入加锁后的区域。解锁操作必须写在finally里，因为需要确保
+                reentrantLock.unlock();   // 解锁，在解锁前其他任何线程都无法进入加锁后的区域。解锁操作必须写在finally里，因为需要确保
                                           // 锁被释放，如果因为中途有异常锁没有被释放，则其他所有线程都无法进入该区域。
             }
         }
